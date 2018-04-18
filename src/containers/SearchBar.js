ebe2203a4cba066ec509801c077c57cf5cf59528
Bhavika Bhalani <bbhalani@albany.edu>
2018-04-18 10:44:46 -0400

    added google map preview

--- src/containers/SearchBar.js
+++ src/containers/SearchBar.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import { fetchWeather } from '../actions/index'
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
     constructor(props){
         super(props);
         this.state = { term: '' };
@@ -12,9 +15,15 @@ export default class SearchBar extends Component {
 
     }
 
+    formSubmitHandler = (event) => {
+        event.preventDefault();
+        this.props.fetchWeather(this.state.term);
+        this.setState({term: ''});
+    }
+
     render(){
         return(
-            <form className="input-group">
+            <form onSubmit={this.formSubmitHandler} className="input-group">
                 <input placeholder="Get a five-day forecast in your favourite cities"
                     className="form-control"
                     value={this.state.term}
@@ -27,3 +36,9 @@ export default class SearchBar extends Component {
         );
     }
 }
+
+function mapDispatchToProps(dispatch){
+    return bindActionCreators({fetchWeather}, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
